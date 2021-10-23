85acbe89f69e5f44eb4f9f6a16d9aa95b36f2cde
Johannes Roth <johannes.roth18199@gmail.com>
2021-10-23 20:14:00 +0200

    press E to open door

---
+++ Door.gd
@@ -0,0 +1,40 @@
+extends Area
+
+
+export var whatToDo = "open door"
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area_body_entered(body):
+	if body.name == "Player": # hardcoded, whatever
+		body.enable_interaction(self)
+		
+	pass
+
+
+func _on_Area_body_exited(body):
+	if body.name == "Player":
+		body.disable_interaction(self)
+	
+	
+	pass # Replace with function body.
+
+
+func on_player_interact(player):
+	print("Player wants to open a door")
+	pass
